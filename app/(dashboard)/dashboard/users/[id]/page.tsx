7e9cbbfcfd96a0e89b48e09198ebd4024c957dca
Manoj Kumar <click4manoj@gmail.com>
2026-06-16 17:53:37 +0530

    Search filter and Pagination in users page

--- app/(dashboard)/dashboard/users/[id]/page.tsx
+++ app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,22 +1,24 @@
 'use client';
+import { ArrowLeft, BackpackIcon, MoveLeft } from "lucide-react";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 interface User {
 	id: number;
 	firstName: string;
 	age: number;
 	address: {
-    address: string;
-    city: string;
-    state: string;
-    country: string;
-  };
+		address: string;
+		city: string;
+		state: string;
+		country: string;
+	};
 	email: string;
 }
 export default function SingleUser({ params }: { params: Promise<{ id: string }> }) {
-		const [userData, setUserData] = useState<User | null>(null);
-		const [isLoading, setIsLoading] = useState(true);
-		const [showSuccess, setShowSuccess] = useState(false);
-		const fetchUser  = async () => {
+	const [userData, setUserData] = useState<User | null>(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [showSuccess, setShowSuccess] = useState(false);
+	const fetchUser = async () => {
 		const { id } = await params;
 		const res = await fetch(`https://dummyjson.com/users/${id}`);
 		const data = await res.json();
@@ -38,71 +40,74 @@ export default function SingleUser({ params }: { params: Promise<{ id: string }>
 	// console.log(data);
 	useEffect(() => {
 		fetchUser();
-		
+
 	}, []);
 	useEffect(() => {
 		if (showSuccess) {
 			const timer = setTimeout(() => {
 				setShowSuccess(false);
-			}, 5000); 
+			}, 5000);
 			return () => clearTimeout(timer);
 		}
-	},[showSuccess])
-if (isLoading) {
-    return <div>Loading...</div>;
-  }
+	}, [showSuccess])
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
 	if (!userData) {
-    return <div>User not found</div>;
-  }
+		return <div>User not found</div>;
+	}
 	return (<>
-	{showSuccess && (
-				<>
-					<div className="fixed top-24  right-4 bg-white flex items-center text-green-700 w-full max-w-xs p-4 text-body bg-neutral-primary-soft rounded-base shadow-xs border border-gray-200 border-default" role="alert">
+		{showSuccess && (
+			<>
+				<div className="fixed top-24  right-4 bg-white flex items-center text-green-700 w-full max-w-xs p-4 text-body bg-neutral-primary-soft rounded-base shadow-xs border border-gray-200 border-default" role="alert">
+					<svg
+						height={24}
+						width={24}
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						strokeWidth={1.5}
+						strokeLinecap="round"
+						strokeLinejoin="round"
+
+					>
+						<path d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+					</svg>
+					<div className="ms-2.5 text-sm ">Updated Successfully!</div>
+					<button onClick={() => setShowSuccess(false)} type="button" className="cursor-pointer ms-auto flex items-center justify-center text-body hover:text-heading bg-transparent box-border border border-transparent hover:bg-neutral-secondary-medium focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded text-sm h-8 w-8 focus:outline-none" data-dismiss-target="#toast-default" aria-label="Close">
+						<span className="sr-only">Close</span>
 						<svg
-							height={24}
-							width={24}
-							viewBox="0 0 24 24"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
 							fill="none"
-							stroke="currentColor"
-							strokeWidth={1.5}
-							strokeLinecap="round"
-							strokeLinejoin="round"
-
+							viewBox="0 0 24 24"
 						>
-							<path d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-						</svg>
-						<div className="ms-2.5 text-sm ">Updated Successfully!</div>
-						<button onClick={() => setShowSuccess(false)} type="button" className="cursor-pointer ms-auto flex items-center justify-center text-body hover:text-heading bg-transparent box-border border border-transparent hover:bg-neutral-secondary-medium focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded text-sm h-8 w-8 focus:outline-none" data-dismiss-target="#toast-default" aria-label="Close">
-							<span className="sr-only">Close</span>
-							<svg
-								aria-hidden="true"
-								xmlns="http://www.w3.org/2000/svg"
-								width="24"
-								height="24"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									d="M6 18 17.94 6M18 18 6.06 6"
-								/>
-							</svg>						</button>
-					</div>
-				</>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M6 18 17.94 6M18 18 6.06 6"
+							/>
+						</svg>						</button>
+				</div>
+			</>
 
-			)}
+		)}
+		<Link href={'/dashboard/users'} className="cursor-pointer inline-flex align-middle items-center gap-2 group relative  py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+		<ArrowLeft width={16} height={16} />
+		Back</Link>
 		<div className="max-w-md w-full space-y-8 mx-auto">
 			<h1 className="text-2xl font-semibold mb-6">Update users {userData.firstName}</h1>
 			<div className="bg-white py-8 px-6 shadow rounded-lg sm:px-10">
 				<form className="space-y-6" onSubmit={handleUpdate}>
 					<div>
 						<label htmlFor="firstName" className="block text-sm font-medium text-gray-700">First Name</label>
-						<input 
-						onChange={(e) => setUserData({ ...userData, firstName: e.target.value })}
-						name="firstName" id="firstName" type="text" defaultValue={userData.firstName} className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+						<input
+							onChange={(e) => setUserData({ ...userData, firstName: e.target.value })}
+							name="firstName" id="firstName" type="text" defaultValue={userData.firstName}  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 						/>
 					</div>
 					<div>
@@ -111,22 +116,22 @@ if (isLoading) {
 						<div id="email-display"
 						>{userData.email}</div>
 					</div>
-						<div>
+					<div>
 						<label htmlFor="age" className="block text-sm font-medium text-gray-700">Age</label>
-						<input 
-						onChange={(e) => setUserData({ ...userData, age: parseInt(e.target.value) })}						
-						name="age" id="age" type="text" defaultValue={userData.age} className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+						<input
+							onChange={(e) => setUserData({ ...userData, age: parseInt(e.target.value) })}
+							name="age" id="age" type="text" defaultValue={userData.age} className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 						/>
 					</div>
 					<div>
 						<label htmlFor="address" className="block text-sm font-medium text-gray-700">Address</label>
-						<input 
-						onChange={(e) => setUserData({ ...userData, address: { ...userData.address, address: e.target.value } })}
-						name="address" id="address" type="text" defaultValue={userData.address.address} className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+						<input
+							onChange={(e) => setUserData({ ...userData, address: { ...userData.address, address: e.target.value } })}
+							name="address" id="address" type="text" defaultValue={userData.address.address} className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 						/>
 					</div>
 					<div>
-						<button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Update</button>
+						<button type="submit" className="cursor-pointer group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Update</button>
 					</div>
 				</form>
 			</div>
--- app/(dashboard)/dashboard/users/page.tsx
+++ app/(dashboard)/dashboard/users/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { SquarePen, Trash } from "lucide-react";
+import { ChevronLeft, ChevronRight, SquarePen, Trash } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -25,6 +25,9 @@ interface Users {
 }
 export default function DashboardUsers() {
   const [users, setUsers] = useState<Users[]>([]);
+  const [search,setSearch] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
   const fetchUsers = async () => {
     const res = await fetch('https://dummyjson.com/users');
     const data = await res.json();
@@ -44,16 +47,41 @@ const handleDelete = async (id: number) => {
     console.error('Delete failed:', err);
   }
 };
+const filteredUsers = users.filter(user => 
+  user.firstName.toLowerCase().includes(search.toLowerCase()) ||
+  user.lastName.toLowerCase().includes(search.toLowerCase()) ||
+  user.email.toLowerCase().includes(search.toLowerCase())
+);
+const totalPages = Math.ceil(filteredUsers.length / itemsPerPage);
+const currentUsers = filteredUsers.slice(
+  (currentPage - 1) * itemsPerPage,
+  currentPage * itemsPerPage
+);
+
+const handlePageChange = (page: number) => {
+  if(page>=1 && page<=totalPages){
+    setCurrentPage(page);
+  }
+}
+
+
   useEffect(() => {
     fetchUsers();
   }, [])
   return (<>
     <div className="flex justify-between">
-
       <h1 className="text-2xl font-semibold mb-6">Users List</h1>
       <Link href={'/dashboard/add-user'} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-6">Add User</Link>
     </div>
-    {users.length > 0 ? (
+    <div>
+      <input type="text" placeholder="Search..." className="border border-gray-300 rounded-md py-2 px-4 mb-4" 
+        onChange={(e) =>{
+          setSearch(e.target.value)
+        }}
+        value={search}
+      />
+    </div>
+    {currentUsers.length > 0 ? (
       <div className="overflow-x-auto">
         <table className="table-auto w-full text-left whitespace-no-wrap">
           <thead>
@@ -68,7 +96,7 @@ const handleDelete = async (id: number) => {
             </tr>
           </thead>
           <tbody>
-            {users.map((user, index) => (
+            {currentUsers.map((user, index) => (
               <tr key={user.id} className="border-b-2 border-l-2 border-r-2 border-gray-300">
                 <td className="text-center ">{index + 1}</td>
                 <td className="px-4 py-3">
@@ -78,7 +106,6 @@ const handleDelete = async (id: number) => {
                 <td className="px-4 py-3">{user.email}</td>
                 <td className="px-4 py-3">{user.address.address}</td>
                 <td className="px-4 py-3">{user.address.country}</td>
-
                 <td className="px-4 py-3 text-right flex gap-1 justify-end">
                   <button className="text-gray-900 cursor-pointer" onClick={() => handleDelete(user.id)}> <Trash /></button>
                   <Link href={`/dashboard/users/${user.id}`} className="text-gray-900 cursor-pointer"> <SquarePen />
@@ -88,6 +115,39 @@ const handleDelete = async (id: number) => {
             ))}
           </tbody>
         </table>
+         {/* Pagination Controls */}
+        <div className="flex justify-between items-center mt-4">
+          <div className="text-gray-600 text-sm">
+            Showing {((currentPage - 1) * itemsPerPage) + 1} to {Math.min(currentPage * itemsPerPage, filteredUsers.length)} of {filteredUsers.length} users
+          </div>
+          <div className="flex gap-2">
+            <button 
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+              className="flex items-center cursor-pointer gap-1 px-3 py-1 border border-gray-300 rounded disabled:opacity-50 disabled:cursor-not-allowed hover:bg-gray-100"
+            >
+              <ChevronLeft size={16} /> Previous
+            </button>
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
+              <button
+                key={page}
+                onClick={() => handlePageChange(page)}
+                className={`px-3 py-1 border cursor-pointer rounded {currentPage === page ? 'bg-white text-gray-700 border-gray-300 hover:bg-gray-100'
+                }`}
+              >
+                {page}
+              </button>
+            ))}
+            
+            <button 
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+              className="flex items-center cursor-pointer gap-1 px-3 py-1 border border-gray-300 rounded disabled:opacity-50 disabled:cursor-not-allowed hover:bg-gray-100"
+            >
+              Next <ChevronRight size={16} />
+            </button>
+          </div>
+        </div>
       </div>) : (
       <div className="py-6 text-gray-600">No users found.</div>
     )}
